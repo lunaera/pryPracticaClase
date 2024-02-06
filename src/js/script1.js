c227753b18a1544503575e86bfab83d6fe3132c5
erahin <erahinpachecoluna@hotmail.com>
2024-02-06 13:51:29 -0600

    Primeros cambios al proyecto

---
+++ src/js/script1.js
@@ -0,0 +1,7 @@
+const location1 = 'New york,' + 'NY';
+const location2 = 'New York,' + 'NY';
+const location3 = 'New York,' + 'NY';
+
+console.log(location1);
+console.log(location2);
+console.log(location3);
